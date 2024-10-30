62b4585893f3decec2400cc2e4d9dd472389436e
CS-DaviMagalhaes <davi.eler.magalhaes@gmail.com>
2024-10-30 17:57:10 -0500

    basic testing

--- models/keras_1000_epochs/main.py
+++ models/keras_1000_epochs/main.py
@@ -1,6 +1,7 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
+import time
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -13,6 +14,12 @@ class_names = open("./models/keras_1000_epochs/labels.txt", "r").readlines()
 
 # CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture(0)
+
+# Initialize variables for tracking
+confidence_threshold = 0.80  # 80% confidence
+time_threshold = 2.0  # 2 seconds
+start_time = None
+current_class = None
 
 while True:
     # Grab the webcamera's image.
@@ -31,14 +38,29 @@ while True:
     image = (image / 127.5) - 1
 
     # Predicts the model
-    prediction = model.predict(image)
+    prediction = model.predict(image, verbose=0) #Put verbose=0 to stop keras printing on console
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
+    
+    if confidence_score >= confidence_threshold:
+        if current_class == class_name:
+            # If yes, check the time since the start of high-confidence
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= time_threshold:
+                print(f"Confirmed Class: {class_name} with {confidence_score * 100:.2f}% confidence")
+        else:
+            # If it's a new class, reset the timer
+            current_class = class_name
+            start_time = time.time()
+    else:
+        # Reset if confidence falls below threshold
+        current_class = None
+        start_time = None
 
     # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    #print("Class:", class_name[2:], end="")
+    #print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
@@ -49,5 +71,3 @@ while True:
 
 camera.release()
 cv2.destroyAllWindows()
-
-
